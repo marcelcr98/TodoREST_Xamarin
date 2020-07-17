fefb019a2e2d444fc8089d11119612e7395d6648
marceloarmando <mcuellarr98@gmail.com>
2020-07-17 09:21:35 -0500

    commit 1

---
+++ TodoREST/Data/RestService.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace TodoREST
+{
+    public class RestService : IRestService
+    {
+        HttpClient client;
+
+        public List<TodoItem> Items { get; private set; }
+
+        public RestService()
+        {
+#if DEBUG
+            client = new HttpClient(DependencyService.Get<IHttpClientHandlerService>().GetInsecureHandler());
+#else
+            client = new HttpClient();
+#endif
+        }
+
+        public async Task<List<TodoItem>> RefreshDataAsync()
+        {
+            Items = new List<TodoItem>();
+
+            var uri = new Uri(string.Format(Constants.RestUrl, string.Empty));
+            try
+            {
+                var response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+
+                    var content = await response.Content.ReadAsStringAsync();
+                    //Request es un JSON, te toca deserializar
+                    Items = JsonConvert.DeserializeObject<List<TodoItem>>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
+
+            return Items;
+        }
+
+        public async Task SaveTodoItemAsync(TodoItem item, bool isNewItem = false)
+        {
+            var uri = new Uri(string.Format(Constants.RestUrl, string.Empty));
+
+            try
+            {
+                //Response es un objeto.
+                //Serializar
+                var json = JsonConvert.SerializeObject(item);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = null;
+                if (isNewItem)
+                {
+                    response = await client.PostAsync(uri, content);
+                }
+                else
+                {
+                    response = await client.PutAsync(uri, content);
+                }
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine(@"\tTodoItem successfully saved.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
+        }
+
+        public async Task DeleteTodoItemAsync(string id)
+        {
+            var uri = new Uri(string.Format(Constants.RestUrl, id));
+
+            try
+            {
+                var response = await client.DeleteAsync(uri);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine(@"\tTodoItem successfully deleted.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
+        }
+    }
+}
---
+++ TodoREST/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TodoREST
+{
+	public class TodoItem
+	{
+		public string id { get; set; }
+
+		public string name { get; set; }
+
+		public string lastname { get; set; }
+		public string gender { get; set; }
+		public string birth { get; set; }
+
+	}
+}
